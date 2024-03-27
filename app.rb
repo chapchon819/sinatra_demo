1e30efac1fb32447280e792c241fad9c8bdf9b6b
chapchon819 <l000merci.819@gmail.com>
2024-03-27 22:55:34 +0900

    whetherAPIお試し

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require 'uri'
+require 'net/http'
+require 'faraday'
 
 class App < Sinatra::Base
   configure :development do
@@ -7,6 +10,20 @@ class App < Sinatra::Base
   end
 
   get '/' do
+    options = {
+      url: 'http://api.weatherapi.com'
+    }
+    conn = Faraday.new(options) do |f|
+      f.response :json
+      f.adapter :net_http
+      f.request :url_encoded
+    end
+    req_params = {
+      key: '54fc789584ab4cc5a8e111055242703',
+      q: 'Tokyo'
+    }
+    res = conn.get('/v1?&current.json', req_params)
+    @whether = res.body['current']
     erb :index
   end
 end
